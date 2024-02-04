91391679c0b18ed6d5030081a2740d6455f3ba75
kasperekd <kasperekd@mail.ru>
2024-02-04 17:08:37 +0700

    simple rust server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:8001").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+
+    if let Some(Ok(request_line)) = buf_reader.lines().next() {
+        println!("Incoming connection from: {}\t{}", stream.peer_addr().unwrap(), request_line);
+
+        let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+            ("HTTP/1.1 200 OK", "hello.html")
+        } 
+        else {
+            ("HTTP/1.1 404 NOT FOUND", "404.html")
+        };
+
+        if let Ok(contents) = fs::read_to_string(filename) {
+            let length = contents.len();
+
+            let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+            stream.write_all(response.as_bytes()).unwrap();
+        } 
+        else {
+            println!("Error reading file");
+        }
+    } 
+    else {
+        println!("Error reading request line");
+    }
 }
